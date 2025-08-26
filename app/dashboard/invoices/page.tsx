3550da9d32b36f3d0f3276440acb8c441d59206a
Raza Dar <mrazadar@gmail.com>
2025-08-26 19:02:28 +0500

    Invoices Page

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import Pagination from "@/app/ui/invoices/pagination";
+import Search from "@/app/ui/search";
+import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import Table from "@/app/ui/invoices/table";
+import { lusitana } from "@/app/ui/fonts";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
 
-function InvoicePage() {
-  return <div>InvoicePage</div>;
+export default async function InvoicePage() {
+  return (
+    <div className="clas">
+      <div className="cla">
+        <h1 className="cla">Invoices</h1>
+      </div>
+      <div className="class">
+        <Search placeholder="Search invoices" />
+        <CreateInvoice />
+      </div>
+      {/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense> */}
+      <div className="mt-5 flex w-full justify-center">
+        {/* <Pagination totalPages={totalPages} /> */}
+      </div>
+    </div>
+  );
 }
-
-export default InvoicePage;
